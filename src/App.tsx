1fd5b9acb67223166492c02fdac5a27948392f19
pomeloFellow <122317171+pomeloFellow@users.noreply.github.com>
2025-08-20 16:06:26 -0700

    add temp devlog page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home"
 import About from "./components/About";
+import Devlogs from './components/Devlogs';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>}></Route>
         <Route path='/about' element={<About/>}></Route>
+        <Route path='/devlogs' element={<Devlogs/>}></Route>
       </Routes>
     </>
   )
---
+++ src/components/Devlogs.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Topbar from "./TopBar";
+import "../styles/Devlogs.css";
+
+const Devlogs: React.FC = () => {
+  return(
+    <div className="page_container">
+      <Topbar/>
+      <div className="content_container">
+        Content Comming Soon!!!
+      </div>
+    </div>
+  )
+};
+
+export default Devlogs;
