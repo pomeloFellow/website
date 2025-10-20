bdb43b260a0ed641c19f93af01ca9b1f4bedd7c5
pomeloFellow <122317171+pomeloFellow@users.noreply.github.com>
2025-10-20 16:12:10 -0700

    fixed about overflow

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -9,32 +9,34 @@ import imdb_icon from "../../assets/imdb_icon.svg"
 
 const About: React.FC = () => {
   return (
-    <div className="page_container">
+    <>
       <Topbar/>
       <div className="content_container">
-        <span className="profile_pic_container">
-          <img src={profile_pic}/>
-        </span>
-        <span className="description_container">
-          <p>Likha Pulido is a University of California, Santa Cruz graduate. They majored in Computer Engineering (Systems Programing Focus) and minored in Computer Science. Through this experience, they realized their passion for understanding complex architectures by heart. This excitement reaches beyond the world of software design, exploring the worlds of music, film and video games. </p>
-          <p>They enjoy deciphering how different aspects of these projects interact with one another to produce a final project. This knowledge allows them to contribute ideas that assists these systems, quickly troubleshooting issues along the way. They view the application of their knowledge like solving a puzzle, fun and engaging.</p>
-          <div className="icons">
-            <a href="https://www.linkedin.com/in/likhapulido/"
-                target="_blank">
-              <img src={linkedin_icon}/>
-            </a>
-            <a href="https://github.com/pomeloFellow"
-                target="_blank">
-              <img src={github_icon}/>
-            </a>
-            <a href="https://www.imdb.com/name/nm13529851/"
-                target="_blank">
-              <img src={imdb_icon}/>
-            </a>
-          </div>
-        </span>
+        <div className="inner_content_container">
+          <span className="profile_pic_container">
+            <img src={profile_pic}/>
+          </span>
+          <span className="description_container">
+            <p>Likha Pulido is a University of California, Santa Cruz graduate. They majored in Computer Engineering (Systems Programing Focus) and minored in Computer Science. Through this experience, they realized their passion for understanding complex architectures by heart. This excitement reaches beyond the world of software design, exploring the worlds of music, film and video games. </p>
+            <p>They enjoy deciphering how different aspects of these projects interact with one another to produce a final project. This knowledge allows them to contribute ideas that assists these systems, quickly troubleshooting issues along the way. They view the application of their knowledge like solving a puzzle, fun and engaging.</p>
+            <div className="icons">
+              <a href="https://www.linkedin.com/in/likhapulido/"
+                  target="_blank">
+                <img src={linkedin_icon}/>
+              </a>
+              <a href="https://github.com/pomeloFellow"
+                  target="_blank">
+                <img src={github_icon}/>
+              </a>
+              <a href="https://www.imdb.com/name/nm13529851/"
+                  target="_blank">
+                <img src={imdb_icon}/>
+              </a>
+            </div>
+          </span>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
